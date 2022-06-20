e2bfb20d0152592ee93b60c33a065330510f763b
Afghani Trisna Ramadhan <100565301+afghanitr@users.noreply.github.com>
2022-06-20 23:06:06 +0700

    ALL METHODS DONE EXCLUDING PAYMENT GATEWAY

--- controllers/payment.js
+++ controllers/payment.js
@@ -3,48 +3,66 @@ class PaymentController {
   static async doPayment(req, res, next) {
     const t = await sequelize.transaction();
     try {
+      const { OrderId } = req.params;
       //! INGAT, PAKE QUERY!
       const { WorkshopId, UserId } = req.query;
 
-      const userBalance = await User.update(
-        { balance: balance - req.body.TotalPrice },
-        {
-          where: {
-            id: UserId,
+      const order = await Order.findByPk(OrderId, { transaction: t });
+      if (order.paymentType === "cash") {
+        await Order.update(
+          {
+            paymentStatus: true,
           },
-          transaction: t,
-        }
-      );
+          {
+            where: {
+              id: OrderId,
+            },
+            transaction: t,
+          }
+        );
+      } else {
+        const user = await User.findByPk(UserId, { transaction: t });
+        const userBalance = await User.update(
+          { balance: user.balance - order.totalPrice },
+          {
+            where: {
+              id: UserId,
+            },
+            transaction: t,
+          }
+        );
 
-      const balance = await Workshop.update(
-        {
-          balance: balance + req.body.TotalPrice,
-        },
-        {
-          where: {
-            id: WorkshopId,
+        const workshop = await Workshop.findByPk(WorkshopId, { transaction: t });
+        const balance = await Workshop.update(
+          {
+            balance: workshop.balance + order.totalPrice,
           },
-          transaction: t,
-        }
-      );
+          {
+            where: {
+              id: WorkshopId,
+            },
+            transaction: t,
+          }
+        );
 
-      const updateStatus = await Order.update(
-        {
-          paymentStatus: true,
-        },
-        {
-          where: {
-            id: req.params.OrderId,
+        const updateStatus = await Order.update(
+          {
+            paymentStatus: true,
           },
-        },
-        { transaction: t }
-      );
-
+          {
+            where: {
+              id: req.params.OrderId,
+            },
+          },
+          { transaction: t }
+        );
+      }
       await t.commit();
       res.status(200).json({
         message: "Success",
       });
     } catch (error) {
+      console.log(error);
       t.rollback();
       next(error);
     }
