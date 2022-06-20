826d0e41c8a99cf204a3b098cb0a505a9348e480
Afghani Trisna Ramadhan <100565301+afghanitr@users.noreply.github.com>
2022-06-20 19:35:50 +0700

    Fixed bug

--- routes/customers.js
+++ routes/customers.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const CustomerController = require("../controllers/customers.js");
 const router = express.Router();
-const { authn } = require("../middleware/authn");
+const authn = require("../middleware/authn");
 
 router.post("/register", CustomerController.register);
-router.post("/login", CustomerController.loginCustumer);
+router.post("/login", CustomerController.loginCustomer);
 router.use(authn);
 router.patch("/broadcast", CustomerController.updateBroadcast);
 router.get('/near-workshop', CustomerController.findWorkshopByRadius);
